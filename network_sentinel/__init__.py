8e65cece8771c526d63ad36f949d23273351568f
D3F417 <paradoxoxkox@gmail.com>
2024-11-17 01:17:20 -0800

    Update __init__.py

--- network_sentinel/__init__.py
+++ network_sentinel/__init__.py
@@ -1,7 +1,7 @@
 """Network Sentinel - Advanced Network Security Monitoring Tool
 Created by D3F417 (RastaKhiz Team)
-Version: 2.0.0-beta"""
+Version: 2.1.0"""
 
-__version__ = "2.0.0-beta"
+__version__ = "2.1.0"
 __author__ = "D3F417"
 __team__ = "RastaKhiz Team" 
